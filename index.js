1d184e3be3349b76cb5e71d75212b08bec62f3f7
hansdav <hdavidson21@gmail.com>
2023-01-28 15:13:04 +0100

    dino animation added

--- index.js
+++ index.js
@@ -6,4 +6,25 @@ const DINO_EL = document.querySelector('#dino');
 
 function main() {}
 
+DINO_EL.addEventListener('animationend', function () {
+	DINO_EL.classList.remove('jump');
+});
+
+document.addEventListener('keydown', () => {
+	DINO_EL.classList.add('jump');
+});
+
+// document.addEventListener('keydown', (event) => {
+// 	if (event.code === 'Space') {
+// 		DINO_EL.style.bottom = parseInt(DINO_EL.style.bottom || 40) + 100 + 'px';
+// 		console.log('hi');
+// 	}
+// });
+
+// document.addEventListener('keyup', (event) => {
+// 	if (event.code === 'Space' && DINO_EL.style.bottom === '140px') {
+// 		DINO_EL.style.bottom = parseInt(DINO_EL.style.bottom) - 100 + 'px';
+// 	}
+// });
+
 main();
